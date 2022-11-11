da23acadfd8253679c17262e1687215c59bfe79f
show <s.kubota.sys22@morijyobi.ac.jp>
2022-11-11 09:45:04 +0900

    説明文の記載

--- Bmi.java
+++ Bmi.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Bmi{
     public static void main(String[] args) {
      
             Scanner sc = new Scanner(System.in);
@@ -11,6 +11,16 @@ public class Main {
 
             Double BMI = weight / (height*height);
             System.out.println("BMI：" + String.format("%.2f",BMI));
+
+		
+   		 Scanner sc1 = new Scanner(System.in);
+            System.out.print("身長(m)：");
+            Double height1 = sc1.nextDouble();
+            System.out.print("体重(kg)：");
+            Double weight1 = sc1.nextDouble();
+
+            Double BMI1 = weight1 / (height1*height1);
+            System.out.println("BMI：" + String.format("%.2f",BMI1));
     }
 }
 
